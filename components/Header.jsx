dfc732564f07e4ca4d2472b19edbf3be39fc4f9b
Junnygram <you@example.com>
2023-02-22 15:28:20 +0100

    created sidebar and header

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between px-4 pt-4 py-2">
+      <h2>Dashboard</h2>
+      <h2>Welcome Back, Olaleye</h2>
+    </div>
+  );
+}
+
+export default Header;
